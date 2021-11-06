24e42df5b65c9d1ef8e9f5bb248866629734b2ea
morghull <morghull.study@gmail.com>
2021-11-07 01:09:13 +0200

    complete task, first approach

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,29 @@
 'use strict';
 
-const resp = fetch('./assets/data/fruits.json');
-console.log(resp);
+let fruits;
+
+fetch('./assets/data/fruits.json')
+  .then((response) => response.json())
+  .then((data) => {
+    fruits = [...data];
+  })
+  .finally(() => {
+    console.log(
+      'We have those fruits in stock: ',
+      fruits.map(({ fruit }) => fruit).join(', ')
+    );
+    console.log(
+      'Red fruits are: ',
+      fruits
+        .filter(({ color }) => color === 'Red')
+        .map(({ fruit }) => fruit)
+        .join(', ')
+    );
+    console.log(
+      'Large fruits are: ',
+      fruits
+        .filter(({ size }) => size === 'Large')
+        .map(({ fruit }) => fruit)
+        .join(', ')
+    );
+  });
